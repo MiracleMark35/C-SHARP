a41ab0154e300bf6d412f3bf936c6359e6338906
MiracleMark35 <markianfrost123@gmail.com>
2026-06-22 19:08:00 +0800

    ForEach If Else Structure

---
+++ Week_1_Foundational_Concepts/Program.cs
@@ -0,0 +1,62 @@
+﻿// initialize variables - graded assignments 
+int currentAssignments = 5;
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+
+
+int sophiaSum = 0;
+int andrewSum = 0;
+int emmaSum = 0;
+int loganSum = 0;
+
+decimal sophiaScore;
+decimal andrewScore;
+decimal emmaScore;
+decimal loganScore;
+
+foreach (var Score in sophiaScores)
+{
+    sophiaSum += Score;
+}
+foreach (var Score in andrewScores)
+{
+    andrewSum += Score;
+}
+
+foreach (var Score in emmaScores)
+{
+    emmaSum += Score;
+}
+
+foreach (var Score in loganScores)
+{
+    loganSum += Score;
+}
+
+
+sophiaScore = (decimal)sophiaSum / currentAssignments;
+andrewScore = (decimal)andrewSum / currentAssignments;
+emmaScore = (decimal)emmaSum / currentAssignments;
+loganScore = (decimal)loganSum / currentAssignments;
+
+foreach (string name in studentNames)
+{
+    Console.WriteLine($"{name}");
+
+}
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+// Console.WriteLine("Press the Enter key to continue");
+// Console.ReadLine();
+
